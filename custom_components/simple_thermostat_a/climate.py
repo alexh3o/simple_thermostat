617b040c40af4107fb467a64fadd8d76c59c6c6d
alexh3o <alexh3o@gmail.com>
2024-02-15 13:48:32 +0100

    Update climate.py

--- custom_components/simple_thermostat_a/climate.py
+++ custom_components/simple_thermostat_a/climate.py
@@ -74,19 +74,6 @@ _LOGGER = logging.getLogger(__name__)
 DEFAULT_TOLERANCE = 0.3
 DEFAULT_NAME = "Generic Thermostat"
 
-CONF_HEATER = "heater"
-CONF_SENSOR = "target_sensor"
-CONF_MIN_TEMP = "min_temp"
-CONF_MAX_TEMP = "max_temp"
-CONF_TARGET_TEMP = "target_temp"
-CONF_AC_MODE = "ac_mode"
-CONF_MIN_DUR = "min_cycle_duration"
-CONF_COLD_TOLERANCE = "cold_tolerance"
-CONF_HOT_TOLERANCE = "hot_tolerance"
-CONF_KEEP_ALIVE = "keep_alive"
-CONF_INITIAL_HVAC_MODE = "initial_hvac_mode"
-CONF_PRECISION = "precision"
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(const.CONF_HEATER): cv.entity_id,
