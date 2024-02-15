02c0f217f59d87fbbbba89b27c5a1b5ff9c742ee
alexh3o <alexh3o@gmail.com>
2024-02-15 15:08:10 +0100

    Create const.py

---
+++ custom_components/simple_thermostat_a/const.py
@@ -0,0 +1,18 @@
+"""Constants for Smart Thermostat"""
+DOMAIN = "smart_thermostat"
+CONF_HEATER = "heater"
+CONF_SENSOR = "target_sensor"
+CONF_AC_MODE = "ac_mode"
+CONF_MAX_TEMP = "max_temp"
+CONF_MIN_DUR = "min_dur"
+CONF_MIN_TEMP = "min_temp"
+DEFAULT_MIN_DUR = '00:00:00'
+DEFAULT_NAME = "Smart Thermostat"
+CONF_HOT_TOLERANCE = "hot_tolerance"
+CONF_DEFAULT_TOLERANCE = 0.3
+CONF_COLD_TOLERANCE = "cold_tolerance"
+CONF_TARGET_TEMP = "target_temp"
+CONF_KEEP_ALIVE = "keep_alive"
+CONF_INITIAL_HVAC_MODE = "initial_hvac_mode"
+CONF_PRECISION = "precision"
+CONF_UNIQUE_ID = "unique_id"
